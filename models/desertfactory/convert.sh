8e0a92d7510e63af93077eda760f64dfe3b2bdff
Rudolf Polzer <divverent@alientrap.org>
2010-10-01 13:11:06 +0200

    update converter

--- models/desertfactory/convert.sh
+++ models/desertfactory/convert.sh
@@ -1,1 +1,3 @@
 find . -name \*.ase -print0 | xargs -0 perl -pi -e 's,textures/desertfactory_,models/desertfactory/textures/,g'
+find . -name \*.ase -print0 | xargs -0 perl -pi -e 's,textures\\desertfactory_,models\\desertfactory\\textures\\,g'
+find . -name \*.obj -print0 | xargs -0 perl -pi -e 's,textures/desertfactory_,models/desertfactory/textures/,g'
