c0020336f04492abcc15a98b3252988ddca257c7
Rudolf Polzer <divverent@alientrap.org>
2010-11-02 00:01:41 +0100

    also allow textures/logos/* (only in logos.shader)

--- scripts/shader-audit.sh
+++ scripts/shader-audit.sh
@@ -149,7 +149,7 @@ use_texture()
 			esac
 			;;
 		## RULE: textures/FOO/* must use textures/FOO/*, for FOO in decals, liquids_water, liquids_slime, liquids_lava
-		textures/decals/*|textures/liquids_*/*|textures/effects_*/*|textures/screens/*)
+		textures/decals/*|textures/liquids_*/*|textures/effects_*/*|textures/screens/*|textures/logos/*)
 			pre=`echo "$1" | cut -d / -f 1-2`
 			case "$2" in
 				"$pre"/*)
